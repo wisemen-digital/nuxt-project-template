5f2b90fb063949c4c2e938519f22f10d34f86b5c
Robbe <robbevaes95@gmail.com>
2024-05-15 14:24:29 +0200

    chore: auth fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { AuthService } from '~/modules/auth/api/services/auth.service'
-
-const userInfo = await AuthService.getCurrentUser()
 </script>
 
 <template>
   <div class="h-screen w-screen">
-    <Container class="flex h-full flex-col items-center justify-center">
-      {{ userInfo }}
+    <AppContainer class="flex h-full flex-col items-center justify-center">
       <h1 class="flex items-center gap-4 text-white">
         <img
           src="/favicon.ico"
@@ -15,6 +11,6 @@ const userInfo = await AuthService.getCurrentUser()
         <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
         <span>Wisemen Nuxt Project Template</span>
       </h1>
-    </Container>
+    </AppContainer>
   </div>
 </template>
