249d717b426e8ed1ad3f7a9d62a124ae26beb8e1
Robbe <robbevaes95@gmail.com>
2024-07-15 13:55:26 +0200

    chore: lint

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import { useCart } from '@cart/api/cart/queries/useCart'
-
 definePageMeta({
   // middleware: 'auth-middleware',
 })
-
-const cart = useCart()
 </script>
 
 <template>
